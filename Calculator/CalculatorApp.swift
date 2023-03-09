ebe2fc0a83e830ec74d89c1b9b4448e8bbcfd09a
Shin ain <s22028@gsm.hs.kr>
2023-03-09 17:16:26 +0900

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 신아인 on 2023/03/07.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
